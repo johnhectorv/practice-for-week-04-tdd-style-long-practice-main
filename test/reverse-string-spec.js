69565eef4e9bf4de4f9769c6dfb701b311d1930d
TIFFANY <tiffanyliu2008@yahoo.com>
2023-05-24 21:36:50 -0700

    tiffany input

--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,15 @@
-// Your code here
+const reverseString = require('../problems/reverse-string.js');
+const { expect } = require('chai');
+describe ('reverseString', () => {
+    it('should reverse string', () => {
+        const input = 'fun';
+        const expected = 'nuf';
+        const result = reverseString(input);
+        expect(result).to.equal(expected);
+    })
+
+    it('should throw error', () => {
+        const input = 123;
+        expect(() => reverseString(input).to.throw(TypeError))
+    })
+})
